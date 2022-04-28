c3011a5a89c2bc681e3049ae04dc331fac4d669e
gaston correa <gastoncorrea90e@gmail.com>
2022-04-29 00:21:42 +0100

    Se creo rutas de request para cargar y consultar usuario

--- src/main/java/com/curriculum/dinamico/controller/UsuarioController.java
+++ src/main/java/com/curriculum/dinamico/controller/UsuarioController.java
@@ -3,7 +3,9 @@ package com.curriculum.dinamico.controller;
 
 import com.curriculum.dinamico.model.Usuario;
 import com.curriculum.dinamico.service.UsuarioService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,10 @@ public class UsuarioController {
     public String crearUsuario(@RequestBody Usuario usuario){
         uService.crearUsuario(usuario);
         return "El usuario fue creado correctamente";
+    }
+    
+    @GetMapping("/usuario/consultar")
+    public List<Usuario> traerUsuarios(){
+        return uService.consultarUsuarios();
     }
 }
