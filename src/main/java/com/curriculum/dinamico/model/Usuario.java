59a5dd7bf8a5cb8dbaa8e3e29af196937c4a2486
gaston correa <gastoncorrea90e@gmail.com>
2022-04-28 15:30:41 +0100

    Se mapeo con ORM la clase Usuario y se creo su RepositorioJPA

--- src/main/java/com/curriculum/dinamico/model/Usuario.java
+++ src/main/java/com/curriculum/dinamico/model/Usuario.java
@@ -1,9 +1,21 @@
 
 package com.curriculum.dinamico.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
     
+    @Id
+    @GeneratedValue (strategy = GenerationType.SEQUENCE)
     private Long idusuario;
     private String email;
     private String password;
---
+++ src/main/java/com/curriculum/dinamico/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+
+package com.curriculum.dinamico.repository;
+
+import com.curriculum.dinamico.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    
+}
