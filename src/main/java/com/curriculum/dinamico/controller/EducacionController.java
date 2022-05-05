7e3599f7cfaee91d53f60a229af132f5a2d725d0
gaston correa <gastoncorrea90e@gmail.com>
2022-05-05 04:28:22 +0100

    Se creo las rutas para actualizar y eliminar una educacion

--- src/main/java/com/curriculum/dinamico/controller/EducacionController.java
+++ src/main/java/com/curriculum/dinamico/controller/EducacionController.java
@@ -3,11 +3,17 @@ package com.curriculum.dinamico.controller;
 
 import com.curriculum.dinamico.model.Educacion;
 import com.curriculum.dinamico.service.IEducacionService;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,5 +31,31 @@ public class EducacionController {
     @GetMapping("/educacion/consultar")
     public List<Educacion> traerListaEducacion(){
         return educacionS.obtenerEducacion();
+    }
+    
+    @PutMapping("/educacion/modificar/{id}")
+    public Educacion modificarUnaEducacion(@PathVariable Long id,
+                                            @RequestParam ("nombre_institucion") String nuevoNombre,
+                                            @RequestParam ("fecha_inicio") Calendar nuevaFechaInicio,
+                                            @RequestParam ("fecha_fin") Calendar nuevaFechaFin,
+                                            @RequestParam ("titulo") String nuevoTitulo){
+        
+        Educacion modificarEducacion = educacionS.obtenerUnaEducacion(id);
+        
+        modificarEducacion.setNombre_institucion(nuevoNombre);
+        modificarEducacion.setFecha_inicio(nuevaFechaInicio);
+        modificarEducacion.setFecha_fin(nuevaFechaFin);
+        modificarEducacion.setTitulo(nuevoTitulo);
+        
+        educacionS.crearNuevaeducacion(modificarEducacion);
+        return modificarEducacion;
+        
+        
+    }
+    
+    @DeleteMapping ("/educacion/eliminar/{id}")
+    public String borrarUnaEducacion(@PathVariable Long id ){
+        educacionS.eliminarUnaEducacion(id);
+        return "el registro fue eliminado";
     }
 }
