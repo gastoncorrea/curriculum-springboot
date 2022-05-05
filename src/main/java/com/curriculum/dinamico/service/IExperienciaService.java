5dc42bc817ef35f2ac95465ce64152bc6f1007e3
gaston correa <gastoncorrea90e@gmail.com>
2022-05-05 16:19:39 +0100

    Se creo la implentacion de servicios de Experiencia con JPA

--- src/main/java/com/curriculum/dinamico/service/IExperienciaService.java
+++ src/main/java/com/curriculum/dinamico/service/IExperienciaService.java
@@ -1,10 +1,17 @@
 
 package com.curriculum.dinamico.service;
 
+import com.curriculum.dinamico.model.Exp_laboral;
+import java.util.List;
+
 
 public interface IExperienciaService {
     
-    public void crearExperiecia(Experiencia experiencia);
+    public void crearExperiecia(Exp_laboral experiencia);
     
+    public List<Exp_laboral> traerExperiencias();
     
+    public Exp_laboral encontrarUnaExperiencia(Long id);
+    
+    public void eliminarUnaExperiencia(Long id);
 }
