8d50530e792bda40928d6d037fd5415e1633e972
gaston correa <gastoncorrea90e@gmail.com>
2022-05-27 20:41:58 +0100

    Se crearon todos los paquetes y clases necesarias para implementar JWT

--- src/main/java/com/curriculum/dinamico/controller/UsuarioController.java
+++ src/main/java/com/curriculum/dinamico/controller/UsuarioController.java
@@ -5,39 +5,43 @@ import com.curriculum.dinamico.model.Usuario;
 import com.curriculum.dinamico.service.UsuarioService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin()
 @RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
     
     @Autowired 
     private UsuarioService uService;
     
-    @PostMapping("/usuario/save")
+    @PostMapping("/save")
     public String crearUsuario(@RequestBody Usuario usuario){
         uService.crearUsuario(usuario);
         return "El usuario fue creado correctamente";
     }
     
-    @GetMapping("/usuario/consultar")
+    @GetMapping("/consultar")
     public List<Usuario> traerUsuarios(){
         return uService.consultarUsuarios();
     }
     
-    @DeleteMapping("/usuario/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String borrarUsuario(@PathVariable Long id){
         uService.eliminarUnUsuario(id);
         return "El usuario fue eliminado correctamente";
     }
     
-    @PutMapping ("/usuario/modificar/{id}")
+    @PutMapping ("/modificar/{id}")
     public String modificarUnUsuario(@PathVariable Long id, @RequestParam ("email") String nuevoEmail, @RequestParam ("password") String nuevoPassword){
         
         Usuario findUsuario = uService.encontrarUnUsuario(id);
