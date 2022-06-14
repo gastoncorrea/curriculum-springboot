cfb4060b89c66316bafbd65b82f1e847196298d7
Gaston <gastoncorrea90e@gmail.com>
2022-06-14 15:02:43 -0300

    Se modifico cross origin en autenticacion

--- src/main/java/com/curriculum/dinamico/controller/AutenticacionController.java
+++ src/main/java/com/curriculum/dinamico/controller/AutenticacionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "/*")
+@CrossOrigin(origins = "*")
 public class AutenticacionController {
     
     @Autowired 
