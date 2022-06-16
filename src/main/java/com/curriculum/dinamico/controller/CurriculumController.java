45158d2f29c09a934a7485f984fcc1cff8b2399a
Gaston <gastoncorrea90e@gmail.com>
2022-06-16 20:42:08 -0300

    Se borro importaciones que no se utilizaban en curriculumcontroller

--- src/main/java/com/curriculum/dinamico/controller/CurriculumController.java
+++ src/main/java/com/curriculum/dinamico/controller/CurriculumController.java
@@ -16,17 +16,12 @@ import com.curriculum.dinamico.service.IPersonaService;
 import com.curriculum.dinamico.service.IProyectoService;
 import com.curriculum.dinamico.service.IResidenciaService;
 import com.curriculum.dinamico.service.UsuarioService;
-import com.curriculum.dinamico.util.JwtUtil;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
