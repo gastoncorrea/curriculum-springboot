f698b8335d2ff2230fc6fe03ba106ff7a32ad1ce
gaston correa <gastoncorrea90e@gmail.com>
2022-10-26 19:40:44 +0100

    Prueba de COrs

--- src/main/java/com/curriculum/dinamico/controller/AutenticacionController.java
+++ src/main/java/com/curriculum/dinamico/controller/AutenticacionController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @CrossOrigin(origins = "*")
+@RestController
 public class AutenticacionController {
     
     @Autowired 
