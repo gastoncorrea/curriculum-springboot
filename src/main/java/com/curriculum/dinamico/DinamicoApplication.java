88bc26785595abde0c403b16441d4abb0715cd02
gaston correa <gastoncorrea90e@gmail.com>
2022-10-26 20:23:53 +0100

    Return init cors

--- src/main/java/com/curriculum/dinamico/DinamicoApplication.java
+++ src/main/java/com/curriculum/dinamico/DinamicoApplication.java
@@ -2,9 +2,6 @@ package com.curriculum.dinamico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DinamicoApplication {
@@ -13,15 +10,4 @@ public class DinamicoApplication {
 		SpringApplication.run(DinamicoApplication.class, args);
 	}
         
-                @Bean
-        	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
-	}
-        
-
 }
--- src/main/java/com/curriculum/dinamico/controller/AutenticacionController.java
+++ src/main/java/com/curriculum/dinamico/controller/AutenticacionController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
 @RestController
+@CrossOrigin(origins = "*")
 public class AutenticacionController {
     
     @Autowired 
